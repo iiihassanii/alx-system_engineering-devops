42fc13a1c6896ff194315fca331c619412f4212a
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-25 15:55:13 +0300

    task 2

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,9 +3,9 @@
 a given employee ID, returns information about
  his/her TO list progress.
  and export into csv"""
+import csv
 import requests
 import sys
-import csv
 
 if __name__ == "__main__":
     id = sys.argv[1]
@@ -14,16 +14,14 @@ if __name__ == "__main__":
     # *get todo where userID = id
     todos = requests.get(url + "todos", {"userId": id}).json()
 
-    name = user["name"]
-
-    done = sum(1 for d in todos if d['completed'])  # Count True values
+    name = user.get("name")
 
     file_name = "{}.csv".format(id)
     with open(file_name, 'w') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
 
         for item in todos:
-            completed_status = item['completed']
-            task_title = item['title']
+            completed_status = item.get("completed")
+            task_title = item.get("title")
 
             writer.writerow([id, name, completed_status, task_title])
