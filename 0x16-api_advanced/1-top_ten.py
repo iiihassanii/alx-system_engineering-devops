372fed8651cb5da0fbf040f7c8c1deab8b33e31d
iiihassanii <hassanabdelazim.max@gmail.com>
2024-04-09 15:33:15 +0300

    task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" function that queries the Reddit API
+and prints the titles of the first 10
+hot posts listed for a given subreddit."""
+import requests
+
+
+def top_ten(subreddit):
+    """ function that queries the Reddit API
+    and prints the titles of the first 10
+    hot posts listed for a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, headers={"User-Agent": "Custom"},
+                            params={"limit": "10"})
+
+    if response.status_code == 200:
+        for get_data in response.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
+        print(None)
