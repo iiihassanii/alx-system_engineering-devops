050d33b61ecf56599487842498baa550f1bfe305
iiihassanii <hassanabdelazim.max@gmail.com>
2024-04-09 15:38:07 +0300

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,9 @@ def top_ten(subreddit):
     """ function that queries the Reddit API
     and prints the titles of the first 10
     hot posts listed for a given subreddit."""
+
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+
     response = requests.get(url, headers={"User-Agent": "Custom"},
                             params={"limit": "10"})
 
